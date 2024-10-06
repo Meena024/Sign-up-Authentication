a0ce3c4f9e947492ccd91f66f9cb79b9f377e5a2
Meena024 <maill2rmeena@gmail.com>
2024-10-06 08:25:00 +0530

    addressing the security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/Auth-context";
 
 function App() {
+  const authCtxt = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
-          <HomePage />
+        <Route path="/" exact>
+          {authCtxt.isLoggedIn && <HomePage />}
+          {!authCtxt.isLoggedIn && <Redirect to="/auth" />}
+          {/* <HomePage /> */}
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
+        {!authCtxt.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="/profile">
+          {authCtxt.isLoggedIn && <UserProfile />}
+          {!authCtxt.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        <Route path="*">
+          <Redirect to="/"></Redirect>
         </Route>
       </Switch>
     </Layout>
